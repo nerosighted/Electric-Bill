f6e4edd976726a3160e3bb1b310accdb92264fda
nerosighted <94204807+nerosighted@users.noreply.github.com>
2022-06-27 19:10:38 -0700

    Initial Commit

---
+++ Electric Bill/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Electric Bill
+//
+//  Created by Christian Millar on 6/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
